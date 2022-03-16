0a4f6f6dacec325458254dcf3a96f3286070d222
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2022-03-16 15:23:36 -0600

    cambios

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,9 +18,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  nuevoJuego: string = ''
+
+  persona: Persona = {
+    nombre: 'Ángel',
+    favoritos: [
+      {id: 1, nombre: 'Xcom 2'},
+      {id: 2, nombre: 'Terraria'},
+    ]
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(){
+    console.log('Formulario posteado');
+  }
+
+  eliminar(i:number){
+    this.persona.favoritos.splice(i,1);
+  }
+
+  agregar(){
+    const nuevo : Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({...nuevo});
+    this.nuevoJuego = '';
   }
 
 }
