bfa46c9bae1f4b853e1060892f59d4371d35a5e9
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2022-03-20 17:28:09 -0600

    cambios

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
+  persona = {
+    genero: 'Femenino',
+    notificaciones: true,
+  }
+
+  terminosYCondiciones:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
