63822ecd4558235522f5124b66117cc54f71a1f8
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2022-03-20 18:14:37 -0600

    cambios

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  /*miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTx 4080ti'),
+    precio: new FormControl(1500),
+    existencias: new FormControl(5)
+  })*/
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['RTx 4080ti'],
+    precio: [0],
+    existencias: [0]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
