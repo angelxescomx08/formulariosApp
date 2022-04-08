d11ff417314b6c4a34b2b24b352120c325b1cb0f
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2022-04-08 11:15:12 -0500

    cambios

---
+++ src/app/auth/pages/registro/registro.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styles: [
+  ]
+})
+export class RegistroComponent implements OnInit {
+
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['',[Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email: ['',[Validators.required,Validators.pattern(this.emailPattern)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Angel Hernandez',
+      email: 'jose.angel@gmail.com'
+    })
+  }
+
+  campoNoValido(campo:string){
+    return this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario(){
+    console.log(this.miFormulario)
+    this.miFormulario.markAllAsTouched()
+  }
+
+}
